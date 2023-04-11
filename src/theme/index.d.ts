8cdddf7caa879556bd19cfc534f6c0c36708eef3
haroon97 <haroonrmit@gmail.com>
2023-04-11 16:37:07 +0500

    created sepearate declaration file

---
+++ src/theme/index.d.ts
@@ -0,0 +1,65 @@
+import {
+  backgroundColorPaletteType,
+  foregroundColorPaletteType,
+} from "./colors";
+
+declare module "@mui/material/styles" {
+  interface Theme {
+    background: backgroundColorPaletteType;
+    foreground: foregroundColorPaletteType;
+  }
+
+  interface ThemeOptions {
+    background: backgroundColorPaletteType;
+    foreground: foregroundColorPaletteType;
+  }
+
+  interface PaletteColor {
+    extraLight?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    extraLight?: string;
+  }
+
+  interface TypographyVariants {
+    h7: React.CSSProperties;
+    h8: React.CSSProperties;
+    bodyLarge: React.CSSProperties;
+    bodyLargeBold: React.CSSProperties;
+    bodyMedium: React.CSSProperties;
+    bodyMediumBold: React.CSSProperties;
+    bodySmall: React.CSSProperties;
+    bodySmallBold: React.CSSProperties;
+    bodyXSmall: React.CSSProperties;
+    bodyXSmallBold: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    h7?: React.CSSProperties;
+    h8?: React.CSSProperties;
+    bodyLarge?: React.CSSProperties;
+    bodyLargeBold?: React.CSSProperties;
+    bodyMedium?: React.CSSProperties;
+    bodyMediumBold?: React.CSSProperties;
+    bodySmall?: React.CSSProperties;
+    bodySmallBold?: React.CSSProperties;
+    bodyXSmall?: React.CSSProperties;
+    bodyXSmallBold?: React.CSSProperties;
+  }
+}
+
+declare module "@mui/material/Typography" {
+  interface TypographyPropsVariantOverrides {
+    h7: true;
+    h8: true;
+    bodyLarge: true;
+    bodyLargeBold: true;
+    bodyMedium: true;
+    bodyMediumBold: true;
+    bodySmall: true;
+    bodySmallBold: true;
+    bodyXSmall: true;
+    bodyXSmallBold: true;
+  }
+}
