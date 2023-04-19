584ce4d85e0c3632b43b9302e9c7d747c34b7647
haroon97 <haroonrmit@gmail.com>
2023-04-19 16:04:30 +0500

    added go back button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import { Box, Button, Icon, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Icon,
+  IconButton,
+  SvgIcon,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { HeaderProps } from "./HeaderProps";
 import { CbButton } from "../Primitive/CbButton/CbButton";
+import { ArrowBack } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 export const Header: React.FC<HeaderProps> = ({
   text,
   isHelpButtonEnabled,
 }) => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
   return (
     <Box
       display="flex"
@@ -15,9 +28,15 @@ export const Header: React.FC<HeaderProps> = ({
       alignItems={"center"}
       boxShadow={2}
     >
-      <Box display="flex">
-        <Icon sx={{ marginRight: 1, color: "primary.dark" }}>arrow_back</Icon>
-        <Typography variant="h4">{text}</Typography>
+      <Box display="flex" alignItems="center">
+        <Box marginRight={0.5}>
+          <IconButton size="small" onClick={() => navigate(-1)}>
+            <ArrowBack color="primary" fontSize="medium" />
+          </IconButton>
+        </Box>
+        <Typography variant="h4" color={theme.foreground.highEmphasis}>
+          {text}
+        </Typography>
       </Box>
       {isHelpButtonEnabled && (
         <CbButton variant="outlined" size="small">
